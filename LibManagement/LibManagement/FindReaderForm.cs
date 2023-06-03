051af20b31d979a175be7482ae7a076e56e0e407
JustVvu <nguyenvanvu065@gmail.com>
2023-06-03 17:17:59 +0700

    Test database successfully on FindingReaderForm, change some detail

--- LibManagement/LibManagement/FindReaderForm.cs
+++ LibManagement/LibManagement/FindReaderForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,22 @@ namespace LibManagement
 {
     public partial class FindReaderForm : Form
     {
+        SqlConnection conn;
+        SqlCommand cmd;
+        string connectionString = "Data Source=VU-NGUYEN;Initial Catalog=QUANLYTHUVIEN;Integrated Security=True";
+        SqlDataAdapter adapter;
+        DataTable dt = new DataTable();
+
+        void loadData()
+        {
+            cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM DocGia";
+            adapter = new SqlDataAdapter(cmd);
+            dt.Clear();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
         public FindReaderForm()
         {
             InitializeComponent();
@@ -27,6 +44,9 @@ namespace LibManagement
 
         private void TimDocGiaForm_Load(object sender, EventArgs e)
         {
+            conn = new SqlConnection(connectionString);
+            conn.Open();
+            loadData();
 
         }
     }
